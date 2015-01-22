e00cc5237250bad445874ff0ed531552e94604bc
lhamello <lhamello@gmail.com>
2015-01-22 14:32:49 -0200

    EntityFactory

---
+++ cucumber/src/test/java/lham/projects/cucumber/test/EntityFactory.java
@@ -0,0 +1,9 @@
+package lham.projects.cucumber.test;
+
+import java.util.Calendar;
+
+public class EntityFactory {
+
+	protected String[] IP = { "172.168.0.50", "127.0.0.0", "10.1.1.50", "172.168.240.236", "10.20.30.55" };
+	protected Calendar calendar = Calendar.getInstance();
+}
