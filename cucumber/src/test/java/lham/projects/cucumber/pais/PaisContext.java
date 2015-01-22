45be7838ba696d5bdb90614146e6f9e783644e7f
lhamello <lhamello@gmail.com>
2015-01-22 14:49:23 -0200

    revert

--- cucumber/src/test/java/lham/projects/cucumber/pais/PaisContext.java
+++ cucumber/src/test/java/lham/projects/cucumber/pais/PaisContext.java
@@ -1,10 +1,16 @@
 package lham.projects.cucumber.pais;
 
 import lham.projects.cucumber.continente.Continente;
+import lham.projects.cucumber.continente.ContinenteBD;
 import lham.projects.cucumber.continente.ContinenteFactory;
+import lham.projects.cucumber.continente.ContinenteRN;
 import lham.projects.cucumber.test.BaseIT;
 
 public class PaisContext extends BaseIT {
+	
+	protected static ContinenteBD continenteBD;
+	protected static ContinenteRN continenteRN;
+	private Continente continente;
 	
 	protected static PaisBD paisBD;
 	protected static PaisRN paisRN;
@@ -21,11 +27,31 @@ public class PaisContext extends BaseIT {
     	paisRN = new PaisRN();
     	paisRN.setPaisBD(paisBD);
     	paisRN.setDAO(paisBD);
+    	
+    	continenteBD = new ContinenteBD();
+    	continenteBD.setEntityManager(entityManager);
+    	
+    	continenteRN = new ContinenteRN();
+    	continenteRN.setContinenteBD(continenteBD);
+    	continenteRN.setDAO(continenteBD);
     }
     
-    protected void cadastrarPais(String template) {
-        Pais pais = new PaisFactory().criarPais(template);
+    protected void cadastrarPais(PaisFactory.Template template) {
+        if (getContinente()==null){
+        	continente = continenteRN.insert(new ContinenteFactory().criarContinente("ams"));        	
+        	setContinente(continente);
+        }
+    	Pais pais = new PaisFactory().criarPais(template, continente);
         paisRN.insert(pais);
     }
+    
+    public Continente getContinente() {
+		return continente;
+	}
+
+	public void setContinente(Continente continente) {
+		this.continente = continente;
+	}
+
 
 }
